2e4e319edb3c7310bf627d383aba4a882bad6ddb
eard3n1 <melihsenlen20@gmail.com>
2025-10-22 22:01:30 +0200

    seperated some values to a new json file

--- response_handler.py
+++ response_handler.py
@@ -1,80 +1,33 @@
 from random import choice, randint
+import json
 
 zar = 0
 skip = False
 
+with open("responses.json", "r", encoding="utf-8") as j:
+    RESPONSES = json.load(j)
+
 def get_response(user_input, bos_yapma_orani):
-    global zar, eski_zar, skip, bos_yap
+    global RESPONSES, zar, eski_zar, skip, bos_yap
 
     bos_yap = randint(1, int(100/bos_yapma_orani)) # can be improved with math module
     eski_zar = zar if not skip else eski_zar
     zar = randint(1, 6)
     skip = False
 
-    # CSV pretty soon...
-    ekstra = [
-    f"{zar} geldi",
-    f"hayırlı olsun {zar} geldi",
-    f"{zar} geldi gele gele",
-    f"attım... {zar} geldi",
-    f"{zar} gelir anca",
-    f"hooop {zar} geldi",
-    f"şanslısın {zar} geldi",               
-    f"ne diyim {zar} geldi",
-    f"{zar} gelmediyse serefsizim",
-    f"bahtsızsın {zar} geldi",
-    ]
-
-    yinelenenler = [
-    f"{zar} geldi yine",
-    f"yine {zar} geldi",
-    f"yine gele gele {zar} geldi",
-    f"NASIL OLUR YINEMI {zar}!!!",
-    f"bunu nasıl yaptın yine {zar} geldi",
-    f"o kadar olasılık arasından yine {zar} geldi",
-    ]
-
-    atma = [
-    "yorulmustum zaten",
-    "tm kardes sen at yerime",
-    "atasım yoktu zaten",
-    "atmazdım zaten",
-    "küstüm:(",
-    "üzdün beni",
-    "ayıp ettin",
-    "böyle olmaz atmak istiyordum",
-    ]
-
-    bos_laflar = [
-    "banane atmicam",
-    "bilmem atsammı?",
-    "yok sen at",
-    "canım istemiyor",
-    "hiç atasım yok",
-    "atmazsam ne olur?",
-    "neden bu kadar ciddisin?",
-    "atmak mı? yok canım",
-    "sen at ben izlicem",
-    "cok isterdim ama atamam",
-    "baya yorgunum biliyor musun?",
-    "yok ya atmam",
-    "7 geldi:)", # slkfjdsfsdfs
-    ]
-
     lowered = user_input.lower()                    
-#                                                 We don't care about TDK's rules here
-    if "zar" in lowered and "at" in lowered: #                     ↓                      
+    if "zar" in lowered and "at" in lowered:           
         if "atma" in lowered and not "atmaz" in lowered and not "atmas" in lowered:
             skip = True
-            return choice(atma)
+            return choice(RESPONSES["atma"])
         
         elif bos_yap == 1:
             skip = True
-            return choice(bos_laflar) # neden bu kadar ciddisin:)
+            return choice(RESPONSES["bos_laflar"])
         
         elif zar == eski_zar:
-            return choice(yinelenenler)
+            return choice(RESPONSES["yinelenenler"]).format(zar=zar)
         
         else:
-            return choice(ekstra)
+            return choice(RESPONSES["ekstra"]).format(zar=zar)
     elif "sa" in lowered and "bot" in lowered or "selam" in lowered and "bot" in lowered: return "as"
